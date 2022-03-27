e0fe724c0e0a7c947e10f0700875d8fe47b900dc
Oluwafemi <olukoyaoluwafemi14@gmail.com>
2022-03-27 17:41:29 -0200

    css animation complete

---
+++ main.js
@@ -0,0 +1,21 @@
+const boxes = document.querySelectorAll('.text-box');
+const loader = document.querySelectorAll('.loader');
+const texts = document.querySelectorAll('.text-content');
+
+boxes.forEach(box => {
+    box.addEventListener('animationend',()=>{
+        loader.forEach(loader => { loader.classList.toggle('hidden')});
+        texts.forEach(text => { text.classList.toggle('hidden')});
+    });
+})
+
+
+
+window.addEventListener('DOMContentLoaded',()=>{
+    setTimeout(displayBox,2150);
+})
+
+function displayBox(e){
+    console.log(e)
+    console.log('Took me 2 seconds to do this');
+}
